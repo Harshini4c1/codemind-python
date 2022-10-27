ee69d76ea5d8eb073c96f361123f755a10a0fcb2
Harshini4c1 <84394187+Harshini4c1@users.noreply.github.com>
2022-10-27 15:53:31 +0530

    from math import sqrt def isprime(n):     if( n==1 or n ==0 ):         return False     for i in range(2, int(sqrt(n)+1)):         if(n%i == 0):             return False     return True n = int(input()) l = list(map(int,input().split())) k = int(input()) count=0 for i in l:     if(i<=k):         if(isprime(i) == True):             count +=1 print(count)

---
+++ primes_less_than_k.py
@@ -0,0 +1,17 @@
+from math import sqrt
+def isprime(n):
+    if( n==1 or n ==0 ):
+        return False
+    for i in range(2, int(sqrt(n)+1)):
+        if(n%i == 0):
+            return False
+    return True
+n = int(input())
+l = list(map(int,input().split()))
+k = int(input())
+count=0
+for i in l:
+    if(i<=k):
+        if(isprime(i) == True):
+            count +=1
+print(count) 
