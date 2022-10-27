468a40d001dd702a7908609074b00e62d8a241a6
Harshini4c1 <84394187+Harshini4c1@users.noreply.github.com>
2022-10-27 14:43:10 +0530

    n = int(input()) l =list(map(int,input().split())) a,b = map(int,input().split()) l.sort() k1,k2 = 0,0 for i in range(len(l)):     if(l[i] <= a):         k1 =i     if(l[i] <= b):         k2 = i c =[] for i in range(k1,k2+1):     c.append(l[i]) o =[] for i in l:     if(i not in c):         o.append(i) if(len(o)>0):     print(min(o)) else:     print(-1)

---
+++ minimum_not_between_A_and_B.py
@@ -0,0 +1,22 @@
+n = int(input())
+l =list(map(int,input().split()))
+a,b = map(int,input().split())
+l.sort()
+k1,k2 = 0,0
+for i in range(len(l)):
+    if(l[i] <= a):
+        k1 =i
+    if(l[i] <= b):
+        k2 = i
+c =[]        
+for i in range(k1,k2+1):
+    c.append(l[i])
+o =[]
+for i in l:
+    if(i not in c):
+        o.append(i)
+if(len(o)>0):
+    print(min(o))
+else:
+    print(-1)
+    
